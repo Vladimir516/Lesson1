1720d4f433609da7745b41f23445ff925b1a531d
Vladimir516 <ablogin.vladimir.1996@gmail.com>
2024-07-21 23:34:12 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(1234 % 1000 // 10)
+print(5678 % 5000 // 10)
+print(23 + 67)
+# "4th program"
+print(13.42 == 42.13)
+x=13.42
+print(round(x%1*100))
+y=42.13
+print(int(y))
+x=42.13
+print(round(x%1*100))
+y=13.42
+print(int(y))
+print(42==42)
+print(13==13)
